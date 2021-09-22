09a9d1e14800d9892bb42341e5ce128000dcc6da
Thalia Brito <thaliabrito@MacBook-Pro.local>
2021-09-22 17:01:03 -0400

    Slide in items on scroll

--- myScript.js
+++ myScript.js
@@ -15,3 +15,29 @@ function unhighlight(el){
 	el.classList.remove("highlight")
 }
 
+const sliders = document.querySelectorAll(".slide-in")
+
+const appearOptions = {
+  threshold: 0.25,
+  rootMargin: "0px 0px -250px 0px"
+};
+
+
+const appearOnScroll = new IntersectionObserver(function(
+  entries,
+  appearOnScroll
+) {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      entry.target.classList.add("appear");
+      appearOnScroll.unobserve(entry.target);
+    }
+  });
+},
+appearOptions);
+
+sliders.forEach(slider => {
+  appearOnScroll.observe(slider);
+});
