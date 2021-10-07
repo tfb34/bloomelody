a9169108f0db311d9bf25f86a158cf00075c0d29
Thalia Brito <thaliabrito@MacBook-Pro.home>
2021-10-06 23:24:26 -0400

    Properly size profile images

--- myScript.js
+++ myScript.js
@@ -1,10 +1,10 @@
 document.onreadystatechange = function() {
 	if (document.readyState !== "complete") {
-		document.querySelector("body").style.visibility = "hidden";
-		document.querySelector("#loader").style.visibility = "visible";
+		document.querySelector("body").style.visibility = "hidden"
+		document.querySelector("#loader").style.visibility = "visible"
 	} else {
 		document.querySelector("#loader").style.display = "none";
-		document.querySelector("body").style.visibility = "visible";
+		document.querySelector("body").style.visibility = "visible"
 	}
 };
 
