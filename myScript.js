5fe024bce3605f9318d1149227ea07d413f076fa
Thalia Brito <thaliabrito@MacBook-Pro.home>
2021-10-06 23:14:43 -0400

    Compress images

--- myScript.js
+++ myScript.js
@@ -1,6 +1,13 @@
 document.onreadystatechange = function() {
-  console.log("ready")
-}
+	if (document.readyState !== "complete") {
+		document.querySelector("body").style.visibility = "hidden";
+		document.querySelector("#loader").style.visibility = "visible";
+	} else {
+		document.querySelector("#loader").style.display = "none";
+		document.querySelector("body").style.visibility = "visible";
+	}
+};
+
 
 function toggle(el){
 	el.classList.toggle("hide")
